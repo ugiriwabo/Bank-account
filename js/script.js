3b84c8d92c264467c3ccd0eccdcb9f84d438afe7
ugiriwabo <ugiraconsole@gmail.com>
2019-01-20 04:07:52 +0200

    edit javascript

--- js/script.js
+++ js/script.js
@@ -1,55 +1,46 @@
-var BankAccount = {
-    result: 0,
-    withdraw: function(amount) {
-        this.result = this.result - amount;
-    },
-    deposit: function(amount) {
-        this.result = this.result + amount;
-    }
-    };
-   
-   $(document).ready(function() {
-   
-   
-   var newAccount = Object.create(BankAccount);
-   
-   $("form#initial-deposit").submit(function(event) {
-    event.preventDefault();
-   
-    var inputtedName = $("input#name").val();
-    var inputtedBalance = parseInt($("input#initial").val());
-   
-    newAccount.name = inputtedName;
-    newAccount.deposit(inputtedBalance);
-   
-   
-    $(".result").text(newAccount.result);
-   });
-   
-   
-   $("form#Deposit").submit(function(event) {
-   
-    event.preventDefault();
-   
-   
-    var newBalance = parseInt($("input#deposit").val());
-   
-   
-    newAccount.deposit(newBalance);
-   
-    $(".result").text(newAccount.result);
-   });
-   
-   
-   $("form#withdraw").submit(function(event) {
-    event.preventDefault();
-   
-    var newBalance = parseInt($("input#withdraw").val());
-   
-   
-    newAccount.withdraw(newBalance);
-   
-    $(".result").text(newAccount.result);
-   
-   });
+// business logic
+function BankAccount(name, result) {
+  this.nameOfNewAccount = name;
+  this.initialDeposit = result;
+}
+BankAccount.prototype.balance = function() {
+    return this.nameOfNewAccount + " " + this.initialDeposit;
+}
+// user interface logic
+$(document).ready(function(){
+    $("form#initial-deposit").submit(function(event) {
+      event.preventDefault();
+      var inputtedName = $("input#name").val();
+      var inputtedInitial = $("input#initial").val();
+      var newBalance = new BankAccount(inputtedName + inputtedInitial );
+      $("#result").append("<span id='result'>" + newBalance.nameOfNewAccount+<span id="result"></span>);
+
+      $("input#name").val("");
+      $("input#initial").val("");
+      $(".account").show();
+    });
+
+    $("form#Deposit").submit(function(event) {
+      event.preventDefault();
+      var inputtedName = $("input#deposit").val();
+      var inputtedInitial = $("input#initial").val()
+      var newBalance = new BankAccount(inputtedName + inputtedInitial );
+      $("#result").append("<span id='result'>" + newBalance.nameOfNewAccount+<span id="result"></span>);
+  
+      $("input#deposit").val("");
+      $("input#initial").val("");
+      $(".account").show();
+    });
+    
+    $("form#withdraw").submit(function(event) {
+      event.preventDefault();
+      var inputtedName = $("input#withdraw").val();
+      var inputtedInitial = $("input#initial").val()
+      var newBalance = new Contact(inputtedName - inputtedInitial );
+      $("#result").append("<span id='result'>" + newBalance.nameOfNewAccount+<span id="result"></span>);
+  
+      $("input#withdraw").val("");
+      $("input#initial").val("");
+      $(".account").show();
+    });
 });
